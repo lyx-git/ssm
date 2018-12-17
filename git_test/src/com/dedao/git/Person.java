4d4126269335681ba0ec2a3bc430f0e4cecf39cb
lyx1358083659 <1358083659@qq.com>
2018-12-17 21:02:15 +0800

    添加工程到git

---
+++ git_test/src/com/dedao/git/Person.java
@@ -0,0 +1,19 @@
+package com.dedao.git;
+
+/**
+ * author:admin
+ * Date:2018/12/17
+ * Time:20:58
+ */
+public class Person {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaa");
+        System.out.println("aaaaaaaa");
+        System.out.println("aaaaaaaa");
+        System.out.println("aaaaaaaa");
+        System.out.println("aaaaaaaa");
+        System.out.println("aaaaaaaa");
+        System.out.println("aaaaaaaa");
+        System.out.println("aaaaaaaa");
+    }
+}
